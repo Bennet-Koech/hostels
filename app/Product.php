673ba184c160f256733d1f58ffbb33fdf354ed8b
Bennet-Koech <bennetkoech@gmail.com>
2018-09-09 18:42:03 +0300

    VD III

---
+++ app/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Category;
+
+class Product extends Model
+{
+    protected $fillable=['name', 'description', 'size', 'image', 'category_id'];
+
+    public function category()
+    {
+        $this->belongsTo(Category::class);
+    }
+}
